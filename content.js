2e4b049d225082fffcfcab0f08cb5dcb23f19840
owenmoogk <owenmoogk@gmail.com>
2021-06-04 12:23:27 -0400

    this was the problem the whole time.... :clock1:
    so basically i spent a few hours trying to sus out why it only worked some of the time, there was just a trailing slash missing. I also updated some comments as i am just learning at the moment

--- content.js
+++ content.js
@@ -1,7 +1,12 @@
+// THIS IS RUN EVERY TIME CHROME LOADS A PAGE WHICH MATCHES THE 'MATCHES' IN MANIFEST
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+
+	// if we recieve a message that a tab has been updated and it equals the link, then redirect
 	if (request.message === 'TabUpdated') {
-		if (document.location.href == "https://www.youtube.com/"){
+		if (document.location.href == "https://www.youtube.com/" || document.location.href == "https://www.youtube.com"){
 			window.location.replace("https://www.youtube.com/feed/subscriptions")
 		}
 	}
+
 })
