803252fe72ce15d2633897f7194f281dcd2a0062
owenmoogk <owenmoogk@gmail.com>
2021-06-04 12:25:49 -0400

    comments and cleaned it up :green_book:

--- background.js
+++ background.js
@@ -1,18 +1,17 @@
-chrome.runtime.onInstalled.addListener(function() {
-	// ...
-  
+// THIS SCRIPT IS RUN IN A ISOLATED ENVIRONMENT, SEPERATE FROM THE DOM
+
+chrome.runtime.onInstalled.addListener(function () {
+
+	// if a tab is updated, send a message to the frontend
 	chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-	  // changeInfo object: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/onUpdated#changeInfo
-	  // status is more reliable (in my case)
-	  // use "alert(JSON.stringify(changeInfo))" to check what's available and works in your case
-	  if (changeInfo.status === 'complete') {
-		chrome.tabs.sendMessage(tabId, {
-		  message: 'TabUpdated'
-		});
-	  }
+		if (changeInfo.status === 'complete') {
+			chrome.tabs.sendMessage(tabId, {
+				message: 'TabUpdated'
+			});
+		}
 	})
-  });
-  	chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+
+	chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 		if (request.message === 'number') {
 			console.log('we got a runner')
 		}
