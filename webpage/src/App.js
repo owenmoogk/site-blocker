ef1be4e9ff5429319d851c06583ea9d084c05142
owenmoogk <owenmoogk@gmail.com>
2021-06-07 12:46:41 -0400

    dont need the render function :wrench:

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -42,92 +42,85 @@ export default function App() {
 		}
 	}, [])
 
-	function mainPage() {
-		return (
-			<div>
-				{/* top bar */}
-				<nav class="navbar navbar-custom navbar-fixed-top">
-					<div class="container-fluid">
-						<div class="navbar-header">
-							<a class="navbar-brand" href="#"><span>Block</span> Site</a>
-						</div>
-					</div>
-				</nav>
-				<div className="main">
-					<br />
-					<div className="row" id="dashboard">
-
-						<div className="col-lg-12">
-							<div className="input-group">
-
-								<input type="text" className="form-control input-md" placeholder="Add site to block" onChange={(e) => setNewWebsite(e.target.value)} value={newWebsite} />
-
-								<span className="input-group-btn">
-									<button className="btn btn-primary btn-md" id="submitButton" onClick={() => addWebsite()}>
-										Add
-							</button>
-								</span>
-							</div>
-
-							<br />
-
-							<p>{errorMessage}</p>
-						</div>
-
-						<div className="col-lg-12">
-							<div className="table">
-								<table className="table table-bordered table-hover">
-									<thead>
-										<tr>
-											<th>
-												Blocked List ({websites.length})
-									</th>
-											<th className="action_btns">
-												<div className="btn-group">
-													<button className="btn btn-primary" title="Save changes">
-														<i className="fa fa-save"></i>
-													</button>
-													<button className="btn" title="Clear List">
-														<i className="fa fa-trash"></i>
-													</button>
-												</div>
-											</th>
-
-										</tr>
-									</thead>
-									<tbody>
-										{websites.map((website, i) => {
-											return (
-												<tr key={i}>
-													<td className='site_name'>
-														<a target='_blank'>{website}</a>
-													</td>
-													<td className='td_btn'>
-														<i className="fa fa-minus-circle" onClick={() => removeWebsite(website)}></i>
-													</td>
-												</tr>
-											)
-										})}
-									</tbody>
-								</table>
-							</div>
-
-							<p className="text-center">
-								<button className="btn btn-primary" onClick={() => saveWebsites()}>
-									<i className="fa fa-save"></i> Save changes
-						</button>
-							</p>
-
-						</div>
-
+	return (
+		<div>
+			{/* top bar */}
+			<nav class="navbar navbar-custom navbar-fixed-top">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<a class="navbar-brand" href="#"><span>Block</span> Site</a>
 					</div>
 				</div>
+			</nav>
+			<div className="main">
+				<br />
+				<div className="row" id="dashboard">
+
+					<div className="col-lg-12">
+						<div className="input-group">
+
+							<input type="text" className="form-control input-md" placeholder="Add site to block" onChange={(e) => setNewWebsite(e.target.value)} value={newWebsite} />
+
+							<span className="input-group-btn">
+								<button className="btn btn-primary btn-md" id="submitButton" onClick={() => addWebsite()}>
+									Add
+							</button>
+							</span>
+						</div>
+
+						<br />
+
+						<p>{errorMessage}</p>
+					</div>
+
+					<div className="col-lg-12">
+						<div className="table">
+							<table className="table table-bordered table-hover">
+								<thead>
+									<tr>
+										<th>
+											Blocked List ({websites.length})
+									</th>
+										<th className="action_btns">
+											<div className="btn-group">
+												<button className="btn btn-primary" title="Save changes">
+													<i className="fa fa-save"></i>
+												</button>
+												<button className="btn" title="Clear List">
+													<i className="fa fa-trash"></i>
+												</button>
+											</div>
+										</th>
+
+									</tr>
+								</thead>
+								<tbody>
+									{websites.map((website, i) => {
+										return (
+											<tr key={i}>
+												<td className='site_name'>
+													<a target='_blank'>{website}</a>
+												</td>
+												<td className='td_btn'>
+													<i className="fa fa-minus-circle" onClick={() => removeWebsite(website)}></i>
+												</td>
+											</tr>
+										)
+									})}
+								</tbody>
+							</table>
+						</div>
+
+						<p className="text-center">
+							<button className="btn btn-primary" onClick={() => saveWebsites()}>
+								<i className="fa fa-save"></i> Save changes
+						</button>
+						</p>
+
+					</div>
+
+				</div>
 			</div>
-
-		)
-	}
-
-	return (
-		mainPage()
+		</div>
 	)
 }
